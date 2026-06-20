kaspanet/rusty-kaspa

feat(rpc): get block reward by hash (#927)
* feat(rpc): get block reward by hash

* review

* fix blue work

---------

Co-authored-by: IzioDev <9900846+IzioDev@users.noreply.github.com>

rpc/grpc/core/src/convert/message.rs
@@ -429,6 +429,22 @@ from!(item: RpcResult<&kaspa_rpc_core::GetCurrentBlockColorResponse>, protowire:
     Self { blue: item.blue, error: None }
 });
 
+from!(item: &kaspa_rpc_core::GetBlockRewardInfoRequest, protowire::GetBlockRewardInfoRequestMessage, {
+    Self {
+        hash: item.hash.to_string()
+    }
+});
+from!(item: RpcResult<&kaspa_rpc_core::GetBlockRewardInfoResponse>, protowire::GetBlockRewardInfoResponseMessage, {
+    Self {
+        header: Some((&item.header).into()),
+        block_color: item.block_color.into(),
+        confirmation_count: item.confirmation_count,
+        merging_chain_block_hash: item.merging_chain_block_hash.as_ref().map(|x| x.to_string()),
+        reward_amount: item.reward_amount,
+        error: None,
+    }
+});
+
 from!(item: &kaspa_rpc_core::GetUtxoReturnAddressRequest, protowire::GetUtxoReturnAddressRequestMessage, {
     Self {
         txid: item.txid.to_string(),
@@ -955,6 +971,24 @@ try_from!(item: &protowire::GetCurrentBlockColorRequestMessage, kaspa_rpc_core::
 try_from!(item: &protowire::GetCurrentBlockColorResponseMessage, RpcResult<kaspa_rpc_core::GetCurrentBlockColorResponse>, {
     Self {
         blue: item.blue
+    }
+});
+try_from!(item: &protowire::GetBlockRewardInfoRequestMessage, kaspa_rpc_core::GetBlockRewardInfoRequest, {
+    Self {
+        hash: RpcHash::from_str(&item.hash)?
+    }
+});
+try_from!(item: &protowire::GetBlockRewardInfoResponseMessage, RpcResult<kaspa_rpc_core::GetBlockRewardInfoResponse>, {
+    Self {
+        header: item
+            .header
+            .as_ref()
+            .ok_or_else(|| RpcError::MissingRpcFieldError("GetBlockRewardInfoResponseMessage".to_string(), "header".to_string()))?
+            .try_into()?,
+        block_color: item.block_color.into(),
+        confirmation_count: item.confirmation_count,
+        merging_chain_block_hash: item.merging_chain_block_hash.as_ref().map(|x| RpcHash::from_str(x)).transpose()?,
+        reward_amount: item.reward_amount,
     }
 });
 try_from!(item: &protowire::GetUtxoReturnAddressRequestMessage, kaspa_rpc_core::GetUtxoReturnAddressRequest , {

karlsencoin/rusty-karlsen

(cherry picked from commit 770e3e9d4fd29e56869646193308bf39aeeac3e2)

rpc/grpc/core/src/convert/message.rs
@@ -433,6 +433,22 @@ from!(item: RpcResult<&karlsen_rpc_core::GetCurrentBlockColorResponse>, protowir
     Self { blue: item.blue, error: None }
 });
 
+from!(item: &karlsen_rpc_core::GetBlockRewardInfoRequest, protowire::GetBlockRewardInfoRequestMessage, {
+    Self {
+        hash: item.hash.to_string()
+    }
+});
+from!(item: RpcResult<&karlsen_rpc_core::GetBlockRewardInfoResponse>, protowire::GetBlockRewardInfoResponseMessage, {
+    Self {
+        header: Some((&item.header).into()),
+        block_color: item.block_color.into(),
+        confirmation_count: item.confirmation_count,
+        merging_chain_block_hash: item.merging_chain_block_hash.as_ref().map(|x| x.to_string()),
+        reward_amount: item.reward_amount,
+        error: None,
+    }
+});
+
 from!(item: &karlsen_rpc_core::GetUtxoReturnAddressRequest, protowire::GetUtxoReturnAddressRequestMessage, {
     Self {
         txid: item.txid.to_string(),
@@ -927,6 +943,24 @@ try_from!(item: &protowire::GetCurrentBlockColorRequestMessage, karlsen_rpc_core
 try_from!(item: &protowire::GetCurrentBlockColorResponseMessage, RpcResult<karlsen_rpc_core::GetCurrentBlockColorResponse>, {
     Self {
         blue: item.blue
+    }
+});
+try_from!(item: &protowire::GetBlockRewardInfoRequestMessage, karlsen_rpc_core::GetBlockRewardInfoRequest, {
+    Self {
+        hash: RpcHash::from_str(&item.hash)?
+    }
+});
+try_from!(item: &protowire::GetBlockRewardInfoResponseMessage, RpcResult<karlsen_rpc_core::GetBlockRewardInfoResponse>, {
+    Self {
+        header: item
+            .header
+            .as_ref()
+            .ok_or_else(|| RpcError::MissingRpcFieldError("GetBlockRewardInfoResponseMessage".to_string(), "header".to_string()))?
+            .try_into()?,
+        block_color: item.block_color.into(),
+        confirmation_count: item.confirmation_count,
+        merging_chain_block_hash: item.merging_chain_block_hash.as_ref().map(|x| RpcHash::from_str(x)).transpose()?,
+        reward_amount: item.reward_amount,
     }
 });
 try_from!(item: &protowire::GetUtxoReturnAddressRequestMessage, karlsen_rpc_core::GetUtxoReturnAddressRequest , {